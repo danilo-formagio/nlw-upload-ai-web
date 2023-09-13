ba7fe194aba168807d592fa8b74fa791ef5ad68d
Danilo Formagio <daniloformagio@gmail.com>
2023-09-12 23:13:52 -0300

    Apply dark theme and implement the page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Github } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+
 export function App() {
   return (
-   <h1>Hellow World</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤ no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" /> 
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex-1">
+      </div>
+    </div>
   )
 }
